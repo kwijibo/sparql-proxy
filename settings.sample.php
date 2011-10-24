40b86413e653ce29dbc2797a247e5b3b5d78e322
Keith Alexander <github@keithalexander.co.uk>
2011-10-24 10:40:59 +0100

    added sample settings file

---
+++ settings.sample.php
@@ -0,0 +1,10 @@
+<?php
+
+date_default_timezone_set('Europe/London');
+define('KernelHostName', 'example.com');
+define("bucketID","b-katest-3zp71rvw");
+define('endpoint', 'http://db.cohodo.net/sparql/'.bucketID);
+define('user', 'username');
+define('pass', 'password');
+
+?>
